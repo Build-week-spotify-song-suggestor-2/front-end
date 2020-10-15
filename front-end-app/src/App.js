86c49f457b7794623a9542fd26cebb4ef3ba8b50
Jared Strohschein <jared.strohschein@gmail.com>
2020-10-14 21:45:42 -0400

    Login form functionality

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import NewUser from './components/NewUser';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Spotify Song Suggestor front-end-App</h1>
+      <Login />
+      <NewUser />
     </div>
   );
 }
