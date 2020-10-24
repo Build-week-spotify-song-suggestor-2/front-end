c2ec56123b1c58e18e6f5e7b3fcec06697e458eb
drewmrodriguez <drewmrodriguez@gmail.com>
2020-10-24 10:11:38 -0700

    Updates

---
+++ precache-manifest.e2e7f3550259de6c518582acc6a9c307.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e7b732abf5e8ad46db556605bcccf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b258e755c723a6ab0b9",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "0a64cbfe98a4b7214f1a",
+    "url": "/static/css/main.059c0e3b.chunk.css"
+  },
+  {
+    "revision": "8b258e755c723a6ab0b9",
+    "url": "/static/js/2.cf138892.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.cf138892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a64cbfe98a4b7214f1a",
+    "url": "/static/js/main.37b150c6.chunk.js"
+  },
+  {
+    "revision": "7538059f039b51b5d6b5",
+    "url": "/static/js/runtime-main.2f4beef3.js"
+  },
+  {
+    "revision": "af2f1c3d41e5c398cdf0d0b40ab8a6d2",
+    "url": "/static/media/photo-1582730147924-d92f4da00252.af2f1c3d.jpg"
+  },
+  {
+    "revision": "976e376a5d23d1a6e7ac5814a4971a00",
+    "url": "/static/media/suggestify-logo.976e376a.png"
+  }
+]);
